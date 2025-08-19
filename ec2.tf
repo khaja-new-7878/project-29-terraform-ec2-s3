c0a5f7964de547751786d30bbe079d8d530a2067
khaja-new-7878 <mohiddin.khaja7878@gmail.com>
2025-08-19 10:33:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,39 @@
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0861f4e788f5069dd" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name      = "mykey"
+
+  user_data = file("userdata.sh")
+
+  tags = {
+    Name = "project29-web"
+  }
+}
 
