f0cc15a5766ed809beda65e0b0e1285281b68f26
khaja-new-7878 <mohiddin.khaja7878@gmail.com>
2025-08-19 11:25:14 +0530

    uodated

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI in us-east-1
+  instance_type = "t2.micro"
 
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "mohid-terraform-s3-demo-12345"
+  acl    = "private"
+
+  tags = {
+    Name = "Terraform-S3"
+  }
+}
