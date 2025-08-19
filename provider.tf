ad1e3d761c94d1acb800f4fa4552746bd6861a10
khaja-new-7878 <mohiddin.khaja7878@gmail.com>
2025-08-19 10:28:33 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+  backend "s3" {
+    bucket         = "terraform-backend-bucket-123"
+    key            = "project29/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
 
