f6b833a1e9561efbdae8dc3b0f538179c6ea5582
khaja-new-7878 <mohiddin.khaja7878@gmail.com>
2025-08-19 10:34:08 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,1 +1,13 @@
+resource "aws_s3_bucket" "project_bucket" {
+  bucket = "project29-s3-bucket-123"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name = "project29-s3"
+  }
+}
 
