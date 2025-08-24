13b8af8565dc64b9d40ae711e39f184f92365468
root <root@ip-172-31-35-227.ap-south-1.compute.internal>
2025-08-24 10:57:00 +0000

    updated the changes

--- main.tf
+++ main.tf
@@ -1,11 +1,21 @@
-# Fetch latest Amazon Linux 2 AMI dynamically
+# Fetch the latest Amazon Linux 2 AMI dynamically
 data "aws_ami" "amazon_linux2" {
   most_recent = true
-  owners      = ["841932493636"] # Amazon official account
+  owners      = ["841932493636"] # Amazon official account ID
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]  # General purpose volumes
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
   }
 
   filter {
