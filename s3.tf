964f3c78c7fb5a9f1666b8e8abfc749922cace17
root <root@ip-172-31-35-227.ap-south-1.compute.internal>
2025-08-24 08:10:32 +0000

    updated the changes

--- s3.tf
+++ s3.tf
@@ -1,40 +1,23 @@
 resource "aws_s3_bucket" "project29_bucket" {
   bucket = "project29-s3-bucket-123"
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
 
   tags = {
     Name = "project29-s3"
   }
 }
 
-# Attach a policy so Terraform can use it as backend
-resource "aws_s3_bucket_policy" "project29_bucket_policy" {
+# Attach ACL separately
+resource "aws_s3_bucket_acl" "project29_acl" {
+  bucket = aws_s3_bucket.project29_bucket.id
+  acl    = "private"
+}
+
+# Attach versioning separately
+resource "aws_s3_bucket_versioning" "project29_versioning" {
   bucket = aws_s3_bucket.project29_bucket.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::841932493636:root"
-        }
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          "arn:aws:s3:::project29-s3-bucket-123",
-          "arn:aws:s3:::project29-s3-bucket-123/*"
-        ]
-      }
-    ]
-  })
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
