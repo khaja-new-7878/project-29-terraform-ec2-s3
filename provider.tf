92f1e735031905398bee0405ab0586ff69526564
root <root@ip-172-31-35-227.ap-south-1.compute.internal>
2025-08-24 07:21:25 +0000

    Updated

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-  backend "s3" {
-    bucket         = "terraform-backend-bucket-123"
-    key            = "project29/terraform.tfstate"
-    region         = "us-east-1"     # ✅ fix here
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "project_bucket" {
+resource "aws_s3_bucket" "project29_bucket" {
   bucket = "project29-s3-bucket-123"
   acl    = "private"
 
@@ -9,5 +9,32 @@ resource "aws_s3_bucket" "project_bucket" {
   tags = {
     Name = "project29-s3"
   }
+}
+
+# Attach a policy so Terraform can use it as backend
+resource "aws_s3_bucket_policy" "project29_bucket_policy" {
+  bucket = aws_s3_bucket.project29_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::841932493636:root"
+        }
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::project29-s3-bucket-123",
+          "arn:aws:s3:::project29-s3-bucket-123/*"
+        ]
+      }
+    ]
+  })
 }
 
