2df0f4452e8cfe306135faca349506643058405c
root <root@ip-172-31-35-227.ap-south-1.compute.internal>
2025-08-24 07:50:37 +0000

    updated the changes

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "project29-s3-bucket-123"
+    key            = "project29/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
