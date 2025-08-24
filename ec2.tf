c33df1000bece91e5b009ab0c6e1dbd75044f9cc
root <root@ip-172-31-35-227.ap-south-1.compute.internal>
2025-08-24 10:54:25 +0000

    updated the changes

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,34 @@
+# Security group for EC2
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "project29-ec2-sg"
+  }
+}
+
+# Single EC2 instance
 resource "aws_instance" "project29_ec2" {
   ami           = data.aws_ami.amazon_linux2.id
   instance_type = "t2.micro"
@@ -11,3 +42,4 @@ resource "aws_instance" "project29_ec2" {
     Name = "project29-web"
   }
 }
+
