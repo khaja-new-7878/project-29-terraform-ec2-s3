bfb244c2c0eda3120c916f15c642d466e5077d4a
khaja-new-7878 <mohiddin.khaja7878@gmail.com>
2025-08-24 11:13:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI in us-east-1
+  ami           = "ami-0eac8046c099936b0"
   instance_type = "t2.micro"
 
   tags = {
