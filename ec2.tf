56cb711134514bb79cbaaac9fb8a2ab59ffdb615
root <root@ip-172-31-35-227.ap-south-1.compute.internal>
2025-08-25 14:28:25 +0000

    updated the changes

--- ec2.tf
+++ ec2.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "project29_ec2" {
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   associate_public_ip_address = true
-  key_name                    = "project-29-key" # <-- must exist in AWS
+  key_name                    = "project-3"
 
   user_data = file("userdata.sh")
 
